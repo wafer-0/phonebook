149548da658cdfb2428bfcd4d33138f3fdde1bda
wafer-0 <xyzhang722@gmail.com>
2024-11-20 17:10:15 +0800

    exercises 3.15-3.18 finished

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
+require("dotenv").config()
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
-
-let persons = [
-	{
-		id: "1",
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: "2",
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: "3",
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: "4",
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-]
+const Person = require("./models/person")
+const { default: persons } = require("../frontend/src/services/persons")
 
 app.use(cors())
 app.use(express.json())
@@ -40,66 +20,85 @@ app.use(
 )
 
 app.get("/", (request, response) => {
-	response.send("<p>hello<p/>")
+	return "hello"
 })
 
+// mongodb data
 app.get("/api/persons", (request, response) => {
-	response.json(persons)
+	Person.find({}).then((result) => response.json(result))
 })
 
+// mongodb data
 app.post("/api/persons", (request, response) => {
 	const { name, number } = request.body
-	const exist = persons.find((person) => person.name === name)
-	let msg = {}
-	if (name && number && !exist) {
-		// no error
-		const newId = Math.floor(Math.random() * 100)
-		const newPerson = {
-			id: String(newId),
-			name: String(name),
-			number: String(number),
-		}
-		persons = persons.concat(newPerson)
-		return response.json(persons)
-	} else {
-		if (exist) {
-			msg = { error: "name must be unique" }
-		} else if (!name || !number) {
-			let prob = []
-			if (!name) {
-				prob.push("name")
-			}
-			if (!number) {
-				prob.push("number")
-			}
-			const probStr = prob.join(" & ")
-			msg = { error: `${probStr} missing` }
-		}
-		response.status(400).json(msg)
-	}
+	const person = new Person({
+		name: String(name),
+		number: String(number),
+	})
+
+	person.save().then((person) => {
+		console.log(`add ${person.name} ${person.number} to phonebook`)
+		response.json(person)
+	})
 })
 
+// mongodb data
 app.get("/api/persons/:id", (request, response) => {
-	const id = request.params.id
-	const person = persons.find((person) => person.id === id)
-	if (person) {
-		return response.json(person)
-	} else {
-		response.status(404).end()
-	}
+	Person.findById(request.params.id)
+		.then((person) => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch((error) => {
+			console.log(error)
+			response.status(400).send({ error: "malformatted id" })
+		})
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = request.params.id
-	persons = persons.filter((person) => person.id != id)
-	response.json(persons)
+app.put("/api/persons/:id", (request, response) => {
+	Person.findByIdAndUpdate(request.params.id, request.body, {
+		new: true,
+	}).then((updatePerson) => {
+		response.json(updatePerson)
+	})
 })
 
+app.delete("/api/persons/:id", (request, response, next) => {
+	Person.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			if (result != null) {
+				response.json(result)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch((error) => {
+			next(error)
+		})
+})
+
+// mongodb data
 app.get("/info", (request, response) => {
-	const cnt = persons.length
-	const time = Date(Date.now())
-	response.send(`<p>Phonebook has info for ${cnt} people</p><p>${time}</p>`)
+	Person.countDocuments({}).then((cnt) => {
+		const time = Date(Date.now())
+		response.send(
+			`<p>Phonebook has info for ${cnt} people</p><p>${time}</p>`
+		)
+	})
 })
+
+const errorHandler = (error, request, response, next) => {
+	// console.error(error.name)
+	if (error.name == "CastError") {
+		return response.status(400).send({ error: "malformatted id" })
+	}
+	next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
